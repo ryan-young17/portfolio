acc6f5dcb59e45b7a41835010e06a116e61553c9
Ryan Young <ryanbyoung1@gmail.com>
2023-03-17 16:55:18 -0400

    added content to about page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,18 @@
+import React from 'react';
+import './assets/index.css';
+import headshot from './assets/Headshot.png';
+
 function About() {
     return (
         <div className='min-vh-100'>
-            <p>Hello</p>
+            <div className='row'>
+                <div className='col-3'>
+                    <img src={headshot} alt='Headshot of Ryan Young'></img>
+                </div>
+                <div className='col-9'>
+                    <p>My name is Ryan. I am a full-stack web developer with experience in various programming languages such as HTML, CSS, JavaScript, SQL, NoSQL, etc., and a passion for web design. I also have a background in marketing, writing, and graphic design, giving me an advanced perspective on web applications UI/UX. Blending these skills gives me the unique ability to understand both design and client-expectations, and how they coincide with the structure of the application. I look forward to any future projects, opportunities, and collaborations.</p>
+                </div>
+            </div>
         </div>
     );
 }
